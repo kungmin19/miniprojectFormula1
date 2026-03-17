08ddfae77ef5cf5a776c8f8d2a7f96376f4ef6c6
kungmin19 <natthawutkingsanthia@gmail.com>
2026-03-18 04:14:46 +0700

    Commit

--- Formula1/__init__.py
+++ Formula1/__init__.py
@@ -1,3 +1,4 @@
+import os 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -10,7 +11,12 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'f1_secret_key_123'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///f1_database.db'
+    
+    
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    
+    db_path = os.path.join(os.path.dirname(basedir), 'f1_database.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -19,6 +25,9 @@ def create_app():
 
     login_manager.login_view = 'core.login'
     login_manager.login_message_category = 'info'
+
+    
+    from Formula1.models import User, Driver 
 
     from Formula1.core.routes import core
     from Formula1.driver.routes import driver
